b3c82ab3d6bf7aa2b6150af7f79bb6e791706209
pborisp <popovbs@gmail.com>
2025-03-02 19:59:57 +0300

    Tasks 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,22 @@ public class Main {
         return str;
     }
 
+    public static String wrightFirstEnd(String a, String b) {
+        String c = "";
+        if (a.length() == 0) {
+            c += "@";
+        } else {
+            c += a.charAt(0);
+        }
+        if (b.length() == 0) {
+            c += "@";
+        } else {
+            c += b.charAt(b.length() - 1);
+        }
+
+        return c;
+    }
+
     public static void main(String[] args) {
         // Задача №1;
         System.out.println("Задача №1");
@@ -28,5 +44,12 @@ public class Main {
         str2 = strCenter("<<<>>>", "Привет мир!");
         System.out.println(str2);
         System.out.println();
+
+        //Задача №3
+        System.out.println("Задача №3");
+        String aa, bb;
+        aa = "1";
+        bb = "Привет";
+        System.out.println(wrightFirstEnd(aa, bb));
     }
 }
