efd554ce2f59a0f8010b6898e2ee33f42ae6724d
pborisp <popovbs@gmail.com>
2025-03-02 19:07:53 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static StringBuilder doubleSymbol(String str) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            s.append(str.charAt(i));
+            s.append(str.charAt(i));
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        // Задача №1;
+        System.out.println("Задача №1");
+        StringBuilder str = doubleSymbol("Дублирующий текст!!");
+        System.out.println(str);
     }
 }
