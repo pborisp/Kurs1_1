ee08ec5163eb03d5951302d3c61a319edf75c7bc
pborisp <popovbs@gmail.com>
2025-03-03 00:46:43 +0300

    k 1_8 Tasks

--- src/kurs_1_8/k1_8.java
+++ src/kurs_1_8/k1_8.java
@@ -8,6 +8,26 @@ public class k1_8 {
         return c;
     }
 
+    public static void chetNum(int x) {
+        if (x % 2 == 0) {
+            System.out.println("Число " + x + " является четным");
+        } else {
+            System.out.println("Число " + x + " является нечетным");
+        }
+    }
+
+    public static int kvadrat(int X) {
+        return X *= X;
+    }
+
+    public static long kub(int X) {
+        return (long) Math.pow(X, 3);
+    }
+
+    public static boolean Task5(int X) {
+        return X >= 10;
+    }
+
     public static void main(String[] args) {
         // Задача №1
         System.out.println("Задача №1");
@@ -15,5 +35,13 @@ public class k1_8 {
         int Y = 1;
         System.out.println("Минимальное число: " + minOfTwo(X, Y));
         System.out.println();
+
+        // Задача №2,3,4
+        System.out.println("Задача №2,3,4");
+        int x1 = 3;
+        chetNum(x1);
+        System.out.println(kvadrat(x1));
+        System.out.println(kub(x1));
+        System.out.println(Task5(x1));
     }
 }
