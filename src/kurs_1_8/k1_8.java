ed0f946bdc8f1363ad89a712c3cd699e8bb75661
pborisp <popovbs@gmail.com>
2025-03-03 00:12:14 +0300

    k 1_8 Task 1

--- src/kurs_1_8/k1_8.java
+++ src/kurs_1_8/k1_8.java
@@ -4,13 +4,7 @@ public class k1_8 {
 
     public static int minOfTwo(int x, int y) {
         int c;
-        if (x == y) {
-            c = 0;
-        } else if (x < y) {
-            c = x;
-        } else {
-            c = y;
-        }
+        c = Math.min(x, y);
         return c;
     }
 
@@ -18,11 +12,8 @@ public class k1_8 {
         // Задача №1
         System.out.println("Задача №1");
         int X = 7;
-        int Y = 7;
-        if (minOfTwo(X, Y) == 0) {
-            System.out.println("Числа равны");
-        } else {
-            System.out.println(minOfTwo(X, Y));
-        }
+        int Y = 1;
+        System.out.println("Минимальное число: " + minOfTwo(X, Y));
+        System.out.println();
     }
 }
